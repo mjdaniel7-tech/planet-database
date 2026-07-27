8e084c7625a95d19accad9dc69c3fa1be7cea416
mjdaniel7-tech <mjdaniel7@gmail.com>
2026-07-27 13:04:41 -0400

    Create planets.py

---
+++ planets.py
@@ -0,0 +1,84 @@
+PLANETS = [
+
+    {
+
+        "name": "Mercury",
+
+        "type": "Terrestrial",
+
+        "moons": 0
+
+    },
+
+    {
+
+        "name": "Venus",
+
+        "type": "Terrestrial",
+
+        "moons": 0
+
+    },
+
+    {
+
+        "name": "Earth",
+
+        "type": "Terrestrial",
+
+        "moons": 1
+
+    },
+
+    {
+
+        "name": "Mars",
+
+        "type": "Terrestrial",
+
+        "moons": 2
+
+    },
+
+    {
+
+        "name": "Jupiter",
+
+        "type": "Gas Giant",
+
+        "moons": 95
+
+    },
+
+    {
+
+        "name": "Saturn",
+
+        "type": "Gas Giant",
+
+        "moons": 146
+
+    },
+
+    {
+
+        "name": "Uranus",
+
+        "type": "Ice Giant",
+
+        "moons": 28
+
+    },
+
+    {
+
+        "name": "Neptune",
+
+        "type": "Ice Giant",
+
+        "moons": 16
+    }
+
+  [
+
+      
